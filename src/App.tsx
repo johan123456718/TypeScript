ded46f5ea7a6e76640f97e3cbe0d3e23e00d54aa
challita <challita@kth.se>
2021-10-28 16:51:11 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Button/Button.css'
+import { ButtonCounter } from './Button/ButtonCounter';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className = "App">
+
+      <h1>Super basic project to learn typescript</h1>
+      
+      <div className = "Center">
+        <ButtonCounter>
+          {({count, setCount}) => (
+            <div>
+              <Button className = "Button_Size" variant = "primary" onClick = {() => setCount(count + 1)}>+</Button>
+              <h1>{count}</h1>
+            </div>
+          )}
+        </ButtonCounter>
+      </div>
+      
     </div>
   );
 }
---
+++ src/Button/ButtonCounter.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+interface Props{
+    children: (data: {
+        count: number, 
+        setCount: React.Dispatch<React.SetStateAction<number>>
+    })=> JSX.Element | null;
+}
+export const ButtonCounter: React.FC<Props> = ({children}) =>{
+    const [count, setCount] = useState(0);
+    return(
+        <div>
+            {children({count, setCount})}
+        </div>
+    );
+};
